627041ee87fa789d76a3848699ba7396f020286e
regularlydrunk <caitlin-austin@outlook.com>
2020-03-13 23:08:24 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {createAppContainer, createNavigationContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import StartUp from './screens/Startup'
+import LogIn from './screens/LogIn'
+import SignUp from './screens/SignUp'
+
+const AppStackNav = createStackNavigator ({
+
+  StartUp:{
+    screen: StartUp
+  },
+  LogIn:{
+    screen: LogIn
+  },
+  SignUp:{
+    screen: SignUp
+  },
+
+  { 
+    initialRouteParams:StartUp,
+    headerMode: 'none',
+  }
+
+});
+
+
+//Allows navigation around the app
+const AppContainer = createAppContainer(AppStackNav);
+
+//Tells React to Open the App Contain
+export default AppContainer;
+
+
+
