1d9ddc452e5e09240dc798c70f152b7552cbc602
regularlydrunk <caitlin-austin@outlook.com>
2020-03-17 19:06:51 +0000

    continued work on sign up

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { FlatList, ActivityIndicator, Text, View  } from 'react-native'; 
 
 
-constructor(props){   
+constructor(props)
+{   
     super(props);     
     this.state ={ 
         //For Testing
@@ -13,14 +14,8 @@ constructor(props){
  };
 }
 
-
-
-
-
-
-
-
-createAccount(){
+createAccount()
+{
 fetch('10.0.2.2:3333/tables/chittr_user',
     {
         method: 'POST',
@@ -41,5 +36,196 @@ fetch('10.0.2.2:3333/tables/chittr_user',
   })
   .catch((error) => {
     console.error('An error has occured.', error);
-  })
+  });
 }
+
+
+render()
+{
+
+  return (
+
+    <View style={styles.container}>
+
+      <Text style={styles.TitleText}> New user? Create a new account  </Text>
+
+      <TextInput style={styles.ListText}
+
+        underlineColorAndroid="transparent"
+
+        placeholder="Enter your given name here"
+
+        autoCapitalize="none"
+
+        onChangeText={text => this.setState({ given_name: text })}
+
+      />
+
+
+
+      <TextInput style={styles.ListText}
+
+        underlineColorAndroid="transparent"
+
+        placeholder="Enter your family name here"
+
+        autoCapitalize="none"
+
+        onChangeText={text => this.setState({ family_name: text })}
+
+      />
+
+
+
+      <TextInput style={styles.ListText}
+
+        underlineColorAndroid="transparent"
+
+        placeholder="Enter your email here"
+
+        autoCapitalize="none"
+
+        onChangeText={text => this.setState({ email: text })}
+
+      />
+
+
+
+      <TextInput style={styles.ListText}
+
+        underlineColorAndroid="transparent"
+
+        placeholder="Enter password here"
+
+        autoCapitalize="none"
+
+        secureTextEntry={true}
+
+        onChangeText={text => this.setState({ password: text })}
+
+      />
+
+
+
+      <TouchableOpacity
+
+        style={styles.Button}
+
+        onPress=
+
+        {
+
+          () => this.createAccount()
+
+        }>
+
+        <Text style={styles.ButtonText}> Create account </Text>
+
+      </TouchableOpacity>
+
+      <TouchableOpacity
+
+        style={styles.Button}
+
+        onPress=
+
+        {
+
+          () => this.props.navigation.navigate('LoginPage')
+
+        }>
+
+        <Text style={styles.ButtonText}> Go to log in page </Text>
+
+      </TouchableOpacity>
+
+    </View>
+
+  );
+
+}
+
+
+const styles = StyleSheet.create({
+
+  container: {
+
+    flex: 1,
+
+    justifyContent: 'center',
+
+    backgroundColor: '#FFFFFF'
+
+  },
+
+
+
+  ButtonText: {
+
+    color: 'white',
+
+    fontSize: 28,
+
+    fontWeight: 'bold'
+
+  },
+
+
+
+  TitleText: {
+
+    color: 'black',
+
+    fontSize: 28,
+
+    fontWeight: 'bold',
+
+    textAlign: "center",
+
+    margin: 15
+
+  },
+
+
+
+  ListText: {
+
+    color: 'black',
+
+    borderRadius: 15,
+
+    fontSize: 18,
+
+    textAlign: "center",
+
+    backgroundColor: "#F5F5F5",
+
+    alignItems: 'center',
+
+    margin: 10,
+
+    borderColor: 'black',
+
+    borderWidth: 2,
+
+  },
+
+
+
+  Button: {
+
+    backgroundColor: '#233947',
+
+    padding: 5,
+
+    borderRadius: 15,
+
+    alignItems: 'center',
+
+    margin: 15,
+
+    height: 50,
+
+  },
+
+});
